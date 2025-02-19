ffac5ccaacd4cd6c47cf9b1e9a3513083e3e9991
trevans24 <trevans24@gmail.com>
2025-02-19 11:42:52 -0700

    reset to base, starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const suggestions = {
+  happy: ["joyful", "cheerful", "content"],
+  sad: ["unhappy", "miserable", "downcast"],
+  fast: ["quick", "speedy", "rapid"],
+  slow: ["lethargic", "sluggish", "unhurried"],
 }
 
-export default App;
+function App() {
+  const [text, setText] = useState("")
+
+  return <div className="App">working</div>
+}
+
+export default App
